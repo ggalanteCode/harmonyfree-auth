efca3920204e07e8b532792ed30ba8bb11e00ce0
ggalanteCode <ggalante3@gmail.com>
2026-05-28 09:34:20 +0200

    initial commit

---
+++ src/main/java/com/generation153/harmonyfree/auth/HarmonyfreeAuthApplication.java
@@ -0,0 +1,13 @@
+package com.generation153.harmonyfree.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonyfreeAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmonyfreeAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation153/harmonyfree/auth/HarmonyfreeAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation153.harmonyfree.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmonyfreeAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
